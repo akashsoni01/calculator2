68a672bb2bfabc0b3fa341f94b25f9e4bd7ab443
rdec college <rdeccollege@rdecs-mac-mini-5.local>
2017-11-25 12:05:59 +0530

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by rdec 3 on 25/11/17.
+//  Copyright © 2017 rdec 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
