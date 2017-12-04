c14f8faf76eacab63a8d8d95a276dc67e7ec6fcf
akashsoni <akashsoni8285@gmail.com>
2017-12-04 13:34:39 +0530

    color
    for flip the card

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    private var brain = CalculatorBrain()
+    var userIsInTheMiddleOfTyping=false
+    @IBOutlet weak var _display: UILabel!
+    var display:Double{
+        get{
+            return Double(_display.text!)!
+        }
+        set{
+            _display.text=String(newValue)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func dotButtonTuch(_ sender: UIButton) {
+        if !_display.text!.characters.contains("."){
+            _display.text = _display.text! + "."
+        }
     }
-
-
+    
+    @IBAction func tuchOperation(_ sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping{
+            brain.setOperand(display)
+            userIsInTheMiddleOfTyping=false
+        }
+        if let mathmaticalSymbol = sender.currentTitle{
+            brain.performOperator(mathmaticalSymbol)
+        }
+        if let result=brain.result{
+            display=result
+        }
+        
+    }
+    @IBAction func digit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(userIsInTheMiddleOfTyping){
+            _display.text = _display.text! + digit
+        }
+        else{
+            _display.text = sender.currentTitle!
+            userIsInTheMiddleOfTyping = true
+        }
+    }
 }
-
---
+++ calculator/calculatorBrain.swift
@@ -0,0 +1,89 @@
+//
+//  calculatorBrain.swift
+//  calculator
+//
+//  Created by rdec 3 on 25/11/17.
+//  Copyright © 2017 rdec 3. All rights reserved.
+//
+
+import Foundation
+func fact(_ n:Double) -> Double {
+    if n == 1 {
+        return 1
+    }
+    if n == 0 {
+        return 1
+    }else{
+        return fact(n-1)*n
+    }
+}
+struct  CalculatorBrain {
+    private var accumlator:Double?
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double)->Double)
+        case binaryOperation((Double,Double)->Double)
+        case equals
+    }
+    private var operations: Dictionary<String,Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation(sqrt),
+        "cos" : Operation.unaryOperation(cos),
+        "log" : Operation.unaryOperation(log),
+        "!" : Operation.unaryOperation(fact),
+        "±" : Operation.unaryOperation({-$0}),
+        "+" : Operation.binaryOperation({$0 + $1}),
+        "×" : Operation.binaryOperation({$0 * $1}),
+        "-" : Operation.binaryOperation({$0 - $1}),
+        "÷" : Operation.binaryOperation({$0 / $1}),
+        "%" : Operation.binaryOperation({$0.truncatingRemainder(dividingBy:$1)}),
+        "^" : Operation.binaryOperation(pow),
+        "=" : Operation.equals
+    ]
+    mutating func setOperand(_ operand:Double){
+        accumlator = operand
+    }
+    mutating func performOperator(_ symbol:String){
+        if let operation=operations[symbol]
+        {
+            switch  operation{
+            case .constant(let value):
+                accumlator = value
+            case .unaryOperation(let function):
+                if accumlator != nil{
+                    accumlator = function(accumlator!)
+                }
+                break
+            case .binaryOperation(let fun):
+                if accumlator != nil{
+                    performpandingBinaryOperation = performPandingBinaryOperation(function:fun,firstOperand:accumlator!)
+                    accumlator=nil
+                }
+                break
+            case .equals:
+                if performpandingBinaryOperation != nil && accumlator != nil{
+                    performPandingBinaryOperationFunc()
+                }
+                
+            }
+        }
+        
+    }
+    private var performpandingBinaryOperation:performPandingBinaryOperation?
+    mutating func performPandingBinaryOperationFunc(){
+            accumlator = performpandingBinaryOperation?.performWith(secondOperand: accumlator!)
+    }
+    private struct performPandingBinaryOperation {
+         let function : (Double,Double) -> Double
+         let firstOperand:Double
+        func performWith(secondOperand x:Double) -> Double {
+            return function(firstOperand,x)
+        }
+    }
+    var result : Double? {
+        get{
+            return accumlator
+        }
+    }
+}
